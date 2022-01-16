80638993521730438608c13ac6f38d8a8866550a
LAPTOP-CE9Q5GVC\13475 <mznrmnn@gmail.com>
2022-01-16 13:18:47 -0500

    01162022 1:18pm

--- src/hw01/Myself.java
+++ src/hw01/Myself.java
@@ -3,6 +3,7 @@ package hw01;
 public class Myself {
 	public static void main(String[] args) {
 		System.out.println("Mizanur Munna");
+		System.out.println("I am interested to learn JAVA");
 	}
 
 }
